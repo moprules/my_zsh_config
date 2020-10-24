356a9ca56ee529eba814e46d4eacebc11d7bc5f1
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2020-10-24 21:26:08 +0300

    Добавили установку zsh и oh-myzsh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# устанавливаем zsh
+sudo apt install zsh -y
+
+# ставим oh-myzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # копируем файлы в нужные каталоги
 cp anime.zsh-theme $HOME/.oh-my-zsh/themes/
 cp .zshrc $HOME/
